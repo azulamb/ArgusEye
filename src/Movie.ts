0f0f602fa221c632d5e2c2daa5ac0129846cd9e6
Hiroki <1127785+azulamb@users.noreply.github.com>
2018-04-29 15:09:51 +0900

    Fix

--- src/Movie.ts
+++ src/Movie.ts
@@ -23,7 +23,7 @@ export class Movie
 		} ).then( ( file ) =>
 		{
 			const filepath = './movie_' + file;
-			return Exec( 'ffmpeg -r 5 -i ~/tmp_mv/%04d.jpg ' + filepath ).then( () =>
+			return Exec( 'ffmpeg -r 5 -i ~/tmp_mv/%04d.jpg -c:v h264_omx ' + filepath ).then( () =>
 			{
 				return filepath;
 			} );
