5ddb0d85067e9d6d243293db14ee3fedc6d565c8
Hiroki <1127785+azulamb@users.noreply.github.com>
2018-04-29 12:54:36 +0900

    Fix.

--- src/Movie.ts
+++ src/Movie.ts
@@ -15,10 +15,14 @@ export class Movie
 			files.sort();
 			let count = 0;
 			const p = files.map( ( file ) => { return Exec( 'cp ' + file + ' ~/tmp_mv/' + ('0000' + ( ++count ) ).slice( -4 ) + '.jpg' ) } );
-			return Promise.all( p );
-		} ).then( () =>
+			return Promise.all( p ).then( () =>
+			{
+				const file = files[ 0 ].replace( /[^0-9]/g, '' );
+				return file + '.mp4'
+			} );
+		} ).then( ( file ) =>
 		{
-			return Exec( 'ffmpeg -r 5 -i ~/tmp_mv/%04d.jpg ./movie.mp4' );
+			return Exec( 'ffmpeg -r 5 -i ~/tmp_mv/%04d.jpg ./movie_' + file );
 		} );
 	}
 }
