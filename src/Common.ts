858308dd0ebf9e24c59fd06a89b5ed194f563995
Hiroki <1127785+azulamb@users.noreply.github.com>
2018-04-29 13:06:35 +0900

    Fix

--- src/Common.ts
+++ src/Common.ts
@@ -4,8 +4,10 @@ export function Exec( command: string )
 {
 	return new Promise<{ stdout: string, stderr: string }>( ( resolve, reject ) =>
 	{
+		console.log( command );
 		exec( command, ( error, stdout, stderr ) =>
 		{
+			console.log( error );
 			if ( error ) { return reject( error ); }
 			resolve( { stdout: stdout, stderr: stderr } );
 		} );
