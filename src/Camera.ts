3ee1502b221e98be38730a7c29be5eb9a450c34d
Hiroki <1127785+azulamb@users.noreply.github.com>
2018-04-30 20:11:34 +0900

    Fix black.

--- src/Camera.ts
+++ src/Camera.ts
@@ -1,9 +1,15 @@
 import { Exec } from './Common';
+import * as fs from 'fs';
 
 export class Camera
 {
-	public capture( file: string )
+	public capture( file: string ): Promise<{ stdout: string, stderr: string }>
 	{
-		return Exec( 'fswebcam --skip 40 --no-banner ' + file );
+		return Exec( 'fswebcam --skip 40 --no-banner ' + file ).then( ( data ) =>
+		{
+			const stat = fs.statSync( file );
+			if ( !stat || stat.size < 10000 ) { return this.capture( file ); }
+			return Promise.resolve( data );
+		} );
 	}
 }
